a882fadd11d088de6101740b6eb727f9df2e2c4a
javiervaz01 <javivaz2001@gmail.com>
2022-11-21 20:08:09 +0100

    feat: Registro, Login y logout de usuarios

--- ProyectoDjango/ACMECycling/ACMECycling/urls.py
+++ ProyectoDjango/ACMECycling/ACMECycling/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from producto import views
+#from django.views.generic.base import TemplateView
+#from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.inicio),
+    path("registro/", include("registro.urls")),
     path('productos/producto/<int:id_producto>',views.detalles_productos),
+
+
 ]
