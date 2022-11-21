96402ee145a174cb431b4c3c2bf8e40e22115d3e
Alberto <albertobemo7@gmail.com>
2022-11-21 23:18:28 +0100

    feat: busqueda

--- ProyectoDjango/ACMECycling/producto/views.py
+++ ProyectoDjango/ACMECycling/producto/views.py
@@ -13,22 +13,16 @@ def detalles_productos(request, id_producto):
     producto = get_object_or_404(Producto, id=id_producto)
     return render(request, 'productos/detalles.html', {'producto': producto})
     
-def buscador(request):
-    busqueda = request.GET.get("buscar")
-
-    if busqueda:
-        productos = Producto.objects.filter( Q(nombre__icontains = busqueda) |
-                                             Q(descripcion__icontains = busqueda) |
-                                             Q(categoria__icontains = busqueda)).distinct()
-    
-    return render(request, 'catalogo.html', {'productos':productos})
 
 def listar(request):
     busqueda = request.GET.get("buscar")
+    print(busqueda)
     if busqueda:
-        productos = Producto.objects.filter( Q(nombre__icontains = busqueda) |
-                                             Q(descripcion__icontains = busqueda) |
-                                             Q(categoria__icontains = busqueda)).distinct()
+        pr_name = Producto.objects.filter(nombre__icontains = busqueda)
+        pr_descripcion = Producto.objects.filter(descripcion__icontains = busqueda)
+        pr_categoria = Producto.objects.filter(categoria__icontains = busqueda)
+        pr_fabricante = Producto.objects.filter(fabricante__icontains = busqueda)
+        p = (pr_name | pr_descripcion | pr_categoria | pr_fabricante)
     else:
-        productos = Producto.objects.all()
-    return render(request, 'catalogo.html', {'productos':productos})
+        p = Producto.objects.all()
+    return render(request, 'catalogo.html', {'productos':p})
