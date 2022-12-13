46658a26f7dd66a218525fb4ca8e2e18c9991091
pabsanper <pablitosantospsp@gmail.com>
2022-12-13 21:55:54 +0100

    fix: devoluciones

--- ProyectoDjango/ACMECycling/ACMECycling/settings.py
+++ ProyectoDjango/ACMECycling/ACMECycling/settings.py
@@ -148,6 +148,6 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'acme.cycling.pgpi@gmail.com'
-EMAIL_HOST_PASSWORD = '${{ secrets.GMAIL_PASSWORD }}'
+EMAIL_HOST_PASSWORD = 'ypqgyzvkiffshnwz'
 EMAIL_PORT = 587
 
